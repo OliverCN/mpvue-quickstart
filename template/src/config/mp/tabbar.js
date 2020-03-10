1a5b060e8dee682cb24c6f35d3742b9b2ca07fdc
Oliver <lihouliang@outlook.com>
2020-03-10 22:14:56 +0800

    update

--- template/src/config/mp/tabbar.js
+++ template/src/config/mp/tabbar.js
@@ -2,19 +2,19 @@ module.exports = [
   {
     text: "首页",
     pagePath: "pages/index",
-    iconPath: "static/img/tabs/car.png",
+    iconPath: "static/images/tabs/car.png",
     selectedIconPath: "static/images/tabs/car@selected.png"
   },
   {
     text: "发布",
     pagePath: "pages/pub",
-    iconPath: "static/img/tabs/pub.png",
+    iconPath: "static/images/tabs/pub.png",
     selectedIconPath: "static/images/tabs/add@selected.png"
   },
   {
     text: "我的",
     pagePath: "pages/center",
-    iconPath: "static/img/tabs/mine.png",
+    iconPath: "static/images/tabs/mine.png",
     selectedIconPath: "static/images/tabs/mine@selected.png"
   }
 ]
